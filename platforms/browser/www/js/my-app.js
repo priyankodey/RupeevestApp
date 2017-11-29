3e557b58465397a20fd4652a80900a0bb1daf8d9
priyankodey <priyankodey87@gmail.com>
2017-11-29 18:00:35 +0530

    29112017

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -1,5 +1,6 @@
 // Initialize app
-var myApp = new Framework7();
+//var myApp = new Framework7();
+var myApp = new Framework7({pushState: true,});
 
 
 // If we need to use custom DOM library, let's save it to $$ variable:
